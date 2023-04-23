9de2501bc32710a8b94729f2aff197120561cbf6
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-04-23 22:56:40 +0800

    vue_project

---
+++ 02_vue_project/vue_product_demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <IndexPage></IndexPage>
+</template>
+
+<script>
+import IndexPage from "./components/appContent.vue";
+export default {
+  name: 'App',
+  components: {
+    IndexPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: orangered;
+  margin-top: 60px;
+}
+</style>
---
+++ 02_vue_project/vue_product_demo/src/components/appContent.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<script>
+    export default{
+        name: "IndexPage",
+        data() {
+            return {
+                message: "Quirkybird,welcome to Vue world!"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
