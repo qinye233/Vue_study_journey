4902d33af722f081e6436bbd19833f2d0bb6e99b
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-05-12 23:59:13 +0800

    Composition API first experience

---
+++ 04_vue_compositionapi/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>{{ counter }}</h2>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+</template>
+
+<script>
+import  useCounter  from "./hooks/useCounter";
+    export default{
+        setup() {
+            return {...useCounter()}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
